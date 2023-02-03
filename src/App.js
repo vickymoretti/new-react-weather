bcd441a4b52f1bf4a8431a28e7c52421946380e6
victoria <victoria.moretti@hotmail.es>
2023-02-03 10:26:51 +0000

    starting to create the weather app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
-	return <div className="App">Weather App</div>;
+	return (
+		<div className="App">
+			<div className="container">
+				{" "}
+				<Weather />
+			</div>
+			<footer className="footer">
+				This project was coded by{" "}
+				<a href="https://www.linkedin.com/in/victoria-moretti-934a2325a/">
+					Victoria Moretti
+				</a>{" "}
+				and is <a href="https://github.com/vickymoretti">Open-source</a>
+			</footer>
+		</div>
+	);
 }
